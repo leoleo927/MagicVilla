738ee90db3387bb11ac9208e63aa5c4ba02bca4d
Richard Almendariz <lenfer927@gmail.com>
2024-10-23 17:20:47 -0500

    añadiendo servicio GET y documentación de respuestas

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,40 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            return VillaStore.villalist;
+        }
+
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDto> GetVilla(int id)    
+        {
+            if (id==0) 
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villalist.FirstOrDefault(v => v.Id == id);
+
+            if (villa == null) {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+    }
+}
